60add21d62ec2b7d70688c17ea93d5b23d2edb15
neverborn2013 <83453204+neverborn2013@users.noreply.github.com>
2026-06-23 17:42:54 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -158,7 +158,8 @@ https://polymarket.com/event/highest-temperature-in-new-york-on-june-24-2026
 https://polymarket.com/event/bitcoin-above-105k-on-june-26-2026
 https://polymarket.com/event/ethereum-above-4200-on-june-26-2026
 https://polymarket.com/event/solana-above-210-on-june-26-2026
-https://polymarket.com/vi/sports/atp/atp-blancan-berkiet-2026-06-23
+https://polymarket.com/vi/event/what-price-will-ethereum-hit-on-june-23
+https://polymarket.com/vi/event/what-price-will-bitcoin-hit-on-june-23
 """
 
 def extract_slug(url_str):
