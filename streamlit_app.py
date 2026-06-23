81acc6cba6e51d8bcb06e3c4fdaea58dfe3601dd
neverborn2013 <83453204+neverborn2013@users.noreply.github.com>
2026-06-23 21:18:49 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -153,6 +153,7 @@ https://polymarket.com/vi/event/what-price-will-xrp-hit-on-june-24
 https://polymarket.com/vi/sports/wta/wta-sonmez-dart-2026-06-23
 https://polymarket.com/vi/sports/wta/wta-krueger-knutson-2026-06-23
 https://polymarket.com/vi/sports/wta/wta-bejlek-siegemu-2026-06-22
+https://polymarket.com/vi/sports/wta/wta-sonmez-dart-2026-06-23
 """
 
 def extract_slug(url_str):
